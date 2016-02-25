ce5ee6fb4ccfc48e83dbcb96ada50c788ff00393
Reny Alvarado <renyalvarado@gmail.com>
2016-02-24 22:42:59 -0430

    Add new exercise; macro expansion

--- chapter14/exercises.lisp
+++ chapter14/exercises.lisp
@@ -65,7 +65,7 @@
   (format stream "#<ARC ~A / ~A / ~A>"
 	  (node-name (arc-from arc))
 	  (arc-label arc)
-	  (node-name (art-to arc))))
+	  (node-name (arc-to arc))))
 
 (defun add-arc (from-name label to-name action)
   (let* ((from (find-node from-name))
@@ -99,4 +99,83 @@
     ))
 
 (defun fsm (&optional (starting-point 'start))
-  )
+  (setf *current-node* (find-node starting-point))
+  (do ()
+      ((null (node-outputs *current-node*)))
+    (one-trasition)))
+
+(initialize)
+(defnode start)
+(defnode have-5)
+(defnode have-10)
+(defnode have-15)
+(defnode have-20)
+(defnode end)
+
+(defarc start nickel       have-5  "Clunk!")
+(defarc start dime         have-10 "Clink!")
+(defarc start coin-return  start   "Nothing to return.")
+
+(defarc have-5 nickel      have-10 "Clunk!")
+(defarc have-5 dime        have-15 "Clink!")
+(defarc have-5 coin-return start   "Returned five cents.")
+
+(defarc have-10 nickel      have-15 "Clunk!")
+(defarc have-10 dime        have-20 "Clink!")
+(defarc have-10 coin-return start   "Returned ten cents.")
+
+(defarc have-15 nickel      have-20 "Clunk!")
+;;(defarc have-15 dime        have-20 "Nickel change!")
+(defarc have-15 gum-button  end     "Deliver gum.")
+(defarc have-15 coin-return start   "Returned fifteen cents.")
+
+;;(defarc have-20 nickel       have-20 "Nickel returned.")
+;;(defarc have-20 dime         have-20 "Dime returned")
+(defarc have-20 gum-button   end     "Deliver gum, nickel change.")
+(defarc have-20 mint-button  end     "Deliver mints")
+(defarc have-20 coin-return  start   "Returned twenty cents.")
+
+;;; 14.7
+(defnode have-25)
+
+(defarc start   quarter have-25 "Ker-chunk!")
+
+(defarc have-5  quarter have-25 "Nickel change!")
+
+(defarc have-10 quarter have-25 "Dime returned!")
+
+(defarc have-15 nickel  have-20 "Clunk!")
+(defarc have-15 dime    have-25 "Clink!")
+(defarc have-15 quarter have-25 "A nickel and a dime returned")
+
+(defarc have-20 nickel  have-20 "Clunk!")
+(defarc have-20 dime    have-25 "Nickel returned!")
+
+(defarc have-25 nickel           have-25 "Nickel change!")
+(defarc have-25 dime             have-25 "Dime returned!")
+(defarc have-25 quarter          have-25 "Quarter returned!")
+(defarc have-25 gum-button       end     "Deliver gum, dime change.")
+(defarc have-25 mint-button      end     "Deliver mints, nickel change")
+(defarc have-25 chocolate-button end     "Deliver chocolate")
+(defarc have-25 coin-return      start   "Returned twenty five cents.")
+
+;;; 14.11.a
+(defun compile-arc (arc)
+  `((equal this-input ',(arc-label arc))
+    (format t "~&~A" ,(arc-action arc))
+    (,(node-name (arc-to arc)) (rest input-syms))))
+
+;;; 14.11.b
+(defun compile-node (node &aux (res-arcs
+				(mapcar #'compile-arc (node-outputs node))))
+  `(defun ,(node-name node) (input-syms &aux
+					  (this-input (first input-syms)))
+     (cond ((null input-syms) 'start)
+	   ,@res-arcs
+	   (t (error "No arc from ~A with label ~A."
+		     'start this-input)))))
+;;; 14.11.c
+(defmacro compile-machine ()
+  `(progn ,@(mapcar #'compile-node *nodes*)
+      ))
+
